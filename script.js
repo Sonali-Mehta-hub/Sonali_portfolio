774dce035017e7b4713dfa900dd2cdf5240bf91c
Sonali Mehta <sonalikumari1604@gmail.com>
2025-05-05 22:41:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Typed.js configuration
+const options = {
+    strings: ["Web Developer", "AI Enthusiast", "Web Designer", "Problem Solver"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+  };
+  const typed = new Typed("#typed", options);
+  
